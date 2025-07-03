3f80d3dcebbf740b32999fb8efe68786ff7830b0
david.chaparro@e-o.solutions <david.chaparro@e-o.solutions>
2025-07-03 20:58:46 +0000

    added TS compatibility

--- Model/settings.py
+++ Model/settings.py
@@ -2,7 +2,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    MODEL_WEIGHTS: str = "weights.pt"
+    MODEL_WEIGHTS: str = "best_modsim.torchscript"
 
     model_config = SettingsConfigDict(env_prefix="SATDET_", case_sensitive=False)
 
